1e1d4372a5d55fe661b65928052c3724f0347174
Jorge Uriel Cerecero Estrada <urielcerecero1994@gmail.com>
2023-10-02 12:35:10 -0600

    Update homework m3 c5.sql

--- modulo 3/homework m3 c5.sql
+++ modulo 3/homework m3 c5.sql
@@ -241,10 +241,165 @@ ya quedo */
 
 
 /*Utilizar la funcion provista 'UC_Words' (Homework_Utiles.sql) para modificar a letra capital los campos que contengan descripciones para todas las tablas.
+*/
+select * from canaldeventa;
+update canaldeventa
+set descripcion= UC_words(descripcion);
 
-Chequear la consistencia de los campos precio y cantidad de la tabla de ventas.
+select * from clientes;
+update clientes
+set nombre_y_apellido= UC_words(Nombre_y_apellido);
+update clientes
+set domicilio=uc_words(domicilio);
+update clientes
+set localidad=uc_words(localidad);
 
-Chequear que no haya claves duplicadas, y de encontrarla en alguna de las tablas, proponer una solución.    
+select *from compra;
+select * from empleados;
+select * from gasto;
+select * from productos;
+update productos
+set concepto=uc_words(concepto);
+update productos
+set tipo=uc_words(tipo);
+
+select * from proveedores;
+update proveedores
+set nombre=uc_words(nombre);
+update proveedores
+set direccion=uc_words(direccion);
+update proveedores
+set ciudad=uc_words(ciudad);
+update proveedores
+set estado=uc_words(estado);
+update proveedores
+set pais=uc_words(pais);
+update proveedores
+set departamento=uc_words(departamento);
+
+select * from sucursales;
+select * from tipodegasto;
+
+/*Chequear la consistencia de los campos precio y cantidad de la tabla de ventas.*/
+select * from venta;
+select count(v.precio=p.precio), count(v.precio)from venta v
+join productos p
+on v.idproducto=p.idproducto;
+/*Chequear que no haya claves duplicadas, y de encontrarla en alguna de las tablas, proponer una solución.    
  ya quedo*/
 
-/*me quede en la tala clientes*/
+
+/*Normalización
+Generar dos nuevas tablas a partir de la tabla 'empelado' que contengan las entidades Cargo y Sector.*/
+select distinct(sector) from empleados;
+create table cargo(idcargo int auto_increment, tipodecargo varchar (30), primary key (idcargo));
+INSERT INTO cargo (tipodecargo) 
+SELECT DISTINCT Cargo 
+FROM empleados 
+ORDER BY Cargo;
+
+update empleados e
+join cargo c
+on e.idcargo=c.tipodecargo
+set e.idcargo=c.idcargo;
+alter table empleados change idcargo idcargo int;
+select distinct(idcargo) from empleados;
+
+
+create table sector(idsector int auto_increment, sector varchar(30),primary key (idsector));
+INSERT INTO sector (sector) 
+SELECT DISTINCT sector 
+FROM empleados
+ORDER BY sector;
+
+update empleados e
+join sector s
+on e.sector=s.sector
+set e.sector=s.idsector;
+alter table empleados change sector idsector int;
+
+/*Generar una nueva tabla a partir de la tabla 'producto' que contenga la entidad Tipo de Producto.*/
+create table TipoDeProducto();
+select * from productos;
+create table TipoDeProducto 
+(IdTipoDeProducto int auto_increment, TipoDeProducto varchar (30),primary key(IdTipoDeProducto));
+insert into TipoDeProducto(TipoDeProducto)
+select distinct(tipo) from productos
+order by tipo;
+update productos p
+join tipodeproducto t
+on p.tipo=t.tipodeproducto
+set p.tipo=t.idtipodeproducto;
+alter table productos change tipo IdTipoDeProducto int;
+
+SELECT-- el nombre completo en este campo (APELLIDO NOMBRES)
+SUBSTRING(Nombre_y_Apellido,1,LOCATE(" ",Nombre_y_Apellido)) AS apellido ,
+SUBSTRING(Nombre_y_Apellido,LOCATE(" ",Nombre_y_Apellido)+1,LENGTH(Nombre_y_Apellido) ) AS nombres
+FROM clientes;
+
+SELECT substring(Nombre_y_Apellido,1,length(Nombre_y_Apellido)-1-length(SUBSTRING_INDEX(nombre_y_apellido, ' ', -1))) nombre, SUBSTRING_INDEX(nombre_y_apellido, ' ', -1) apellido from clientes;
+alter table clientes add column nombre varchar(50);
+alter table clientes add column apellido varchar (50);
+update clientes
+set nombre=substring(Nombre_y_Apellido,1,length(Nombre_y_Apellido)-1-length(SUBSTRING_INDEX(nombre_y_apellido, ' ', -1)));
+update clientes
+set apellido =SUBSTRING_INDEX(nombre_y_apellido, ' ', -1);
+delete from clientes 
+where nombre is not null;
+
+select domicilio from clientes;
+
+select * from clientes;
+
+select count(*) from empleados;
+
+update empleados e
+join sucursales s
+on e.sucursal=s.sucursal
+set e.sucursal= s.idsucursal;
+
+alter table empleados change sucursal idsucursal int;
+alter table sucursales change nombre Nombre varchar(25);
+
+CREATE TABLE IF NOT EXISTS `aux_venta` (
+  `IdVenta`				INTEGER,
+  `Fecha` 				DATE NOT NULL,
+  `Fecha_Entrega` 		DATE NOT NULL,
+  `IdCliente`			INTEGER, 
+  `IdSucursal`			INTEGER,
+  `IdEmpleado`			INTEGER,
+  `IdProducto`			INTEGER,
+  `Precio`				FLOAT,
+  `Cantidad`			INTEGER,
+  `Motivo`				INTEGER  		-- El campo motivo nos va a servir para identificar las ventas que tengas problemas
+);
+
+UPDATE venta v JOIN productos p 
+		ON (v.IdProducto = p.IdProducto)
+SET v.Precio = p.Precio
+WHERE v.Precio = 0;
+
+SELECT * FROM venta
+WHERE precio = 0;
+
+SELECT * FROM venta
+WHERE Cantidad = '';
+
+
+INSERT INTO aux_venta (IdVenta, Fecha, Fecha_Entrega, IdCliente, IdSucursal, IdEmpleado, IdProducto, Precio, Cantidad, Motivo)
+SELECT IdVenta, Fecha, Fecha_Entrega, IdCliente, IdSucursal, IdEmpleado, IdProducto, Precio, 0, 1
+FROM venta WHERE Cantidad = '' OR Cantidad IS NULL or cantidad=0;
+
+SELECT * FROM aux_venta;
+
+UPDATE venta SET Cantidad = 1 WHERE Cantidad =0 OR ISNULL(Cantidad); -- Aca modificamos los datos vacios en cantidad
+
+ALTER TABLE venta CHANGE Cantidad Cantidad INT NOT NULL DEFAULT 1;
+
+
+select * from venta
+where cantidad = 1;
+
+
+/*Utilizar la funcion provista 'UC_Words' (Homework_Utiles.sql) para modificar a letra capital los campos que contengan descripciones para todas las tablas.*/
+/*Utilizar el procedimiento provisto 'Llenar_Calendario' (Homework_Utiles.sql) para poblar la tabla de calendario.*/
